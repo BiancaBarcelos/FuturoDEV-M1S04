750327f9a05ef0a583b66d72fd1ab05669868c93
BiancaBarcelos <biancassbarcelos@gmail.com>
2024-02-17 17:06:17 -0300

    feat: exercicio 2

---
+++ ex-2/script.js
@@ -0,0 +1,9 @@
+function numero() {
+    let num1 = window.prompt("Atribua um valor:");
+    console.log(num1);
+    
+    let pBtn = document.getElementById("respostaBtn");
+    pBtn.innerHTML = "Aperte F12 para ver a mensagem";
+}
+
+
