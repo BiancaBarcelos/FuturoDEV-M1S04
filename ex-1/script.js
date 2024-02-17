1558566945cd9093aec4bd042a9ff012e7337a1c
BiancaBarcelos <biancassbarcelos@gmail.com>
2024-02-17 17:06:07 -0300

    feat: exercicio 1

---
+++ ex-1/script.js
@@ -0,0 +1,5 @@
+function mensagem() {
+    console.log("Seja bem vindo as minhas atividades!!");
+    let pBtn = document.getElementById("respostaBtn");
+    pBtn.innerHTML = "Aperte F12 para ver a mensagem";
+}
