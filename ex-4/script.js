0a646fbcaadd13172c816f34342f77ef7727c9ec
BiancaBarcelos <biancassbarcelos@gmail.com>
2024-02-17 17:06:42 -0300

    feat: exercicio 4

---
+++ ex-4/script.js
@@ -0,0 +1,8 @@
+function soma(k,b) {
+    let soma = k + b;
+    
+    console.log(soma);
+    
+    let pBtn = document.getElementById("respostaBtn");
+    pBtn.innerHTML = "Aperte F12 para ver a mensagem";
+}
